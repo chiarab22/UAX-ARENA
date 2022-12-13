9b6514ee273489b36927053af4fbc50b48b75414
Gabriel Pelegri <gpelerii@myuax.com>
2022-12-13 19:59:35 +0100

    Cambios

--- UAX_ARENA/app/src/main/java/com/example/android/uax_arena/Registration.java
+++ UAX_ARENA/app/src/main/java/com/example/android/uax_arena/Registration.java
@@ -8,11 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Registration  extends AppCompatActivity {
+public class Registration extends AppCompatActivity {
 
     DBHelper dbHelper;
     EditText email, password;
     Button signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,37 +21,37 @@ public class Registration  extends AppCompatActivity {
 
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
+        signup = (Button) findViewById(R.id.signup);
         dbHelper = new DBHelper(this);
+        String user = email.getText().toString();
+        String pass = password.getText().toString();
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String user = email.getText().toString();
-                String pass = password.getText().toString();
 
-                if(user.equals("")||pass.equals(""))
+                if (user.equals("") || pass.equals("")) {
                     Toast.makeText(Registration.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
-                else{
-                        Boolean checkuser = dbHelper.checkEmail(user);
-                        if(checkuser==false){
-                            Boolean insert = dbHelper.insertData(user, pass);
-                            if(insert==true){
-                                Toast.makeText(Registration.this, "Registered successfully", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                                startActivity(intent);
-                            }else{
-                                Toast.makeText(Registration.this, "Registration failed", Toast.LENGTH_SHORT).show();
-                            }
+                } else {
+                    Boolean checkuser = dbHelper.checkEmail(user);
+                    if (checkuser == false) {
+                        Boolean insert = dbHelper.insertData(user, pass);
+                        if (insert == true) {
+                            Toast.makeText(Registration.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(Registration.this, "Registration failed", Toast.LENGTH_SHORT).show();
                         }
-                        else{
-                            Toast.makeText(Registration.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
-                        }
+                    } else {
+                        Toast.makeText(Registration.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
                     }
                 }
+            }
         });
-
     }
 }
+
 
 
 
