7d1b110fa88b53f9d4d4a38268dc2bb4950e8d22
chiarab22 <cbocc@myuax.com>
2022-12-14 10:09:15 +0100

    forgot password medio acabado

--- UAX_ARENA/app/src/main/java/com/example/android/uax_arena/Password.java
+++ UAX_ARENA/app/src/main/java/com/example/android/uax_arena/Password.java
@@ -1,13 +1,48 @@
 package com.example.android.uax_arena;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.nio.charset.StandardCharsets;
 
 public class Password  extends AppCompatActivity {
+
+   private EditText email;
+   private Button reset;
+   private DBHelper DB;
+   Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        email = (EditText) findViewById(R.id.mail_reset);
+        reset = (Button) findViewById(R.id.reset);
+        DB = new DBHelper(this);
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = email.getText().toString();
+                Boolean checkuser = DB.checkEmail(mail);
+
+                if(checkuser==true){
+                    intent = new Intent(getApplicationContext(), ResetActivity.class);
+                    intent.putExtra("email",mail);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(Password.this, "Mail does not exists", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
     }
 }
