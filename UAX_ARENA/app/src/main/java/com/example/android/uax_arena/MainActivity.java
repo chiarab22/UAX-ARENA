3d9dfcf6101e97a5bd4f5826d263676a40d10b79
Gabriel Pelegri <gpelerii@myuax.com>
2022-12-14 10:45:12 +0100

    Login terminado

--- UAX_ARENA/app/src/main/java/com/example/android/uax_arena/MainActivity.java
+++ UAX_ARENA/app/src/main/java/com/example/android/uax_arena/MainActivity.java
@@ -15,12 +15,14 @@ public class MainActivity extends AppCompatActivity {
     private Button register_button;
     private Button password_button;
     private Intent intent;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DB = new DBHelper(this);
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
 
@@ -47,14 +49,30 @@ public class MainActivity extends AppCompatActivity {
         entry_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (email.getText().toString().equals("example@example.com") && password.getText().toString().equals("example")) {
+
+                String mail = email.getText().toString();
+                String pass = password.getText().toString();
+
+                if(email.equals("")||pass.equals(""))
+                    Toast.makeText(MainActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.checkMailPassword(mail, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(MainActivity.this, "Sign in successfull", Toast.LENGTH_SHORT).show();
+                        Intent intent  = new Intent(getApplicationContext(), Menu.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(MainActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+               /* if (email.getText().toString().equals("example@example.com") && password.getText().toString().equals("example")) {
                     Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
                     intent = new Intent(MainActivity.this, Menu.class);
                     startActivity(intent);
                 } else {
                     Toast.makeText(MainActivity.this, "LOGIN DENIED", Toast.LENGTH_LONG).show();
-                }
-            }
+                }*/
         });
 
 
